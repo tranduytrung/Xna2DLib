6333908ecc1e6764909a38604aaef3b1c96c23a8
tranduytrung <trdtrung@outlook.com>
2014-08-28 16:22:46 +0700

    adds missing files
    Building.cs
    Template.cs

---
+++ DragonCity/DragonCity/Model/Building.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tranduytrung.DragonCity.Model
+{
+    public abstract class Building : IMapEntity
+    {
+        public abstract Type TemplateType { get; }
+        public abstract int BuyValue { get; }
+        public abstract int SellValue { get; }
+
+        public virtual object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ DragonCity/DragonCity/Template/ITemplate.cs
@@ -0,0 +1,26 @@
+﻿using tranduytrung.Xna.Core;
+using tranduytrung.Xna.Map;
+
+namespace tranduytrung.DragonCity.Template
+{
+    public interface ITemplate
+    {
+        DrawableObject PresentableContent { get; }
+        DrawableObject ContextMenu { get; }
+        /// <summary>
+        /// Call this method when context menu is on
+        /// </summary>
+        void Start();
+        /// <summary>
+        /// Call this method when context menu is off
+        /// </summary>
+        void End();
+
+        /// <summary>
+        /// Bind data to template to prepare visual
+        /// </summary>
+        /// <param name="map">map</param>
+        /// <param name="data">data for applying</param>
+        void ApplyData(IsometricMap map, object data);
+    }
+}
