005fcd91a6f6bf35feeefbe86373e998018e9a34
tranduytrung <trdtrung@outlook.com>
2014-09-10 22:35:08 +0700

    Add missing file ArgressiveLook.cs

---
+++ Dovahkiin/Dovahkiin/Model/Action/AgressiveLook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Dovahkiin.ActionHandler;
+using Dovahkiin.Model.Core;
+
+namespace Dovahkiin.Model.Action
+{
+    public class AgressiveLook : IAction
+    {
+        public Action<IActionHandler> EndCallback { get; set; }
+        public ClanType AlliesClan { get; set; }
+    }
+}
