d3b82e7cfb0ac83af6335e70bf1d0a87f15610fa
tranduytrung <trdtrung@outlook.com>
2014-09-13 00:14:16 +0700

    added missing fuckin files

---
+++ Dovahkiin/Dovahkiin/Model/Core/ICarrierOperatable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dovahkiin.Model.Core
+{
+    public interface ICarrierOperatable
+    {
+        CarrierOperator GetOperator();
+    }
+
+    public sealed class CarrierOperator
+    {
+        public CarrierOperator(ICollection<ICarriable> collection)
+        {
+            Collection = collection;
+        }
+
+        public void Add(ICarriable protoItem)
+        {
+            var itemType = protoItem.GetType();
+            var usableTimes = protoItem is Usable ? ((Usable) protoItem).UsableTimes : -1;
+
+            var item = Collection.FirstOrDefault(i => i.GetType() == itemType);
+
+            if (item == null)
+            {
+                item = (ICarriable)Activator.CreateInstance(itemType);
+                Collection.Add(item);
+            }
+
+            if (usableTimes == -1)
+            {
+                ((Usable)item).UsableTimes = usableTimes;
+            }
+        }
+
+        public ICarriable Get(ICarriable protoItem)
+        {
+            var itemType = protoItem.GetType();
+            var usableTimes = protoItem is Usable ? ((Usable)protoItem).UsableTimes : -1;
+
+            var item = Collection.FirstOrDefault(i => i.GetType() == itemType);
+            if (item == null)
+                return null;
+
+            if (usableTimes == -1)
+                return item;
+
+            var usable = (Usable) item;
+
+            if (usable.UsableTimes >= usableTimes)
+            {
+                Collection.Remove(usable);
+                return usable;
+            }
+
+            usable.UsableTimes -= usableTimes;
+            var newItem = (Usable)Activator.CreateInstance(itemType);
+            newItem.UsableTimes = usableTimes;
+
+            return newItem;
+        }
+
+        public ICollection<ICarriable> Collection { get; private set; }
+    }
+
+}
---
+++ Dovahkiin/Dovahkiin/Model/Item/Coin.cs
@@ -0,0 +1,37 @@
+﻿using Dovahkiin.Constant;
+using Dovahkiin.Model.Core;
+
+namespace Dovahkiin.Model.Item
+{
+    public class Coin : Usable
+    {
+        public override int ResouceId
+        {
+            get { return Textures.BloodPotion; }
+        }
+
+        public override string Name
+        {
+            get { return "Coin"; }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return
+                    "Money is a strange business. People who haven't got it aim it strongly. People who have are full of troubles -(Ayrton Senna).\nCoin is useless";
+            }
+        }
+
+        public override int Value
+        {
+            get { return UsableTimes; }
+        }
+
+        protected override bool ApplyEffect(ICreature target)
+        {
+            return false;
+        }
+    }
+}
