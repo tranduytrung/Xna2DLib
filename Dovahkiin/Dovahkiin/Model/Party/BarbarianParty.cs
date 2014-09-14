d0d6177d45711e6eb08ca194f2679a52fcc34887
tranduytrung <trdtrung@outlook.com>
2014-09-14 13:50:24 +0700

    add minssing files

---
+++ Dovahkiin/Dovahkiin/Model/Party/BarbarianParty.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Dovahkiin.ActionHandler;
+using Dovahkiin.Constant;
+using Dovahkiin.Model.Action;
+using Dovahkiin.Model.Core;
+
+namespace Dovahkiin.Model.Party
+{
+    class BarbarianParty : Party
+    {
+        public override IEnumerable<IAction> GetSuggestionActions(Actor target)
+        {
+            yield return new Attack() {Target = this, Title = "Attack!!"};
+        }
+
+        public override int ResouceId
+        {
+            get { return Textures.Knight; }
+        }
+
+        public BarbarianParty()
+        {
+            MovingSpeed = 100;
+            Clan = ClanType.Human | ClanType.Bandit;
+            AddActionHandler(new AgressiveLookHandler() { SightRange = 300 });
+            AddActionHandler(new ChaseHandler());
+            AddActionHandler(new MoveHandler());
+            var attackHandler = new AttackHandler();
+            AddActionHandler(attackHandler);
+            DoAction(new AgressiveLook() { AlliesClan = ClanType.Bandit });
+        }
+    }
+}
